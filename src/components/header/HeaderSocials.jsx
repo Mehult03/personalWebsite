8b8ea29c95a9e57543f6a69c51257278f79cb7a6
Mehult03 <2003mehult@gmail.com>
2024-05-18 14:16:34 -0400

    Added footer and header

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    
+    <div className="header__socials">
+
+        <a href="www.linkedin.com/in/mehulthakkar23"><BsLinkedin/></a>
+        <a href="https://github.com/Mehult03"><BsGithub/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
